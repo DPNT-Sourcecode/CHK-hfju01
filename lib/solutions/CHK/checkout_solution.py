887337825a2a070ff68441b4e4c59fdd9680a63c
sbx_user1051 <sbx_user1051@169.254.133.185>
2022-02-23 23:05:30 +0000

    Wed Feb 23 23:05:30 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
+from sre_parse import SPECIAL_CHARS
+
+
 SKU = {
     "A": 50,
     "B": 30,
@@ -10,16 +13,35 @@ SKU = {
     "D": 15,
 }
 
+SKU_SPECIAL = {
+    "A": (3, 30),
+    "B": (2, 15),
+}
+
 def checkout(skus):
     """
     Return price from sku
     """
     try:
-        return SKU[skus]
+        # split skus amd sum
+        total = 0
+        special_count = {"A": 0, "B": 0} #FIXME
+        for sku in skus:
+            if sku in SKU_SPECIAL:
+                special_count[sku] += 1
+                if(special_count[sku] % SKU_SPECIAL[sku][0] == 0):
+                    total += SKU_SPECIAL[sku][1]
+                else:
+                    total += SKU[sku]
+            else:
+                total += SKU[sku]
+
+        return total
     except KeyError:
         # Add logger
         return -1
 
     
+
 
 
--- test/solution_tests/CHK/test_hello.py
+++ test/solution_tests/CHK/test_hello.py
@@ -10,9 +10,13 @@ class TestSum():
         assert checkout_solution.checkout("AA") == 100
         assert checkout_solution.checkout("AAA") == 130
         assert checkout_solution.checkout("AAAA") == 180
-
+        assert checkout_solution.checkout("AAAAB") == 210
+        assert checkout_solution.checkout("AABBAA") == 225
+        assert checkout_solution.checkout("C") == 20
+        assert checkout_solution.checkout("CCD") == 65
         assert checkout_solution.checkout("xB") == -1
         assert checkout_solution.checkout("test") == -1
         
 
         
+
