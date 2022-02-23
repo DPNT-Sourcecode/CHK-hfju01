31ece07ce197587fba6cafc46a9b2742a215c797
sbx_user1051 <sbx_user1051@169.254.3.69>
2022-02-23 01:13:43 +0000

    Wed Feb 23 01:13:43 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,23 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    return x + y
+    """
+    Return x + y.
+
+    param[0] = a positive integer between 0-100
+    param[1] = a positive integer between 0-100
+    """
+    try:
+        # Check type
+        first_num = int(x)
+        second_num = int(y)
+
+        if first_num < 0 or first_num > 100 or second_num < 0 or second_num > 100:
+            return
+
+    except ValueError:
+        # Logger
+        return
+
+    return first_num + second_num
+
 
