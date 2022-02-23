e2111825c18eebec343a9a3837fff455f9673529
sbx_user1051 <sbx_user1051@169.254.3.69>
2022-02-23 01:37:43 +0000

    Wed Feb 23 01:37:43 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,17 +3,23 @@ from lib.solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
+        """
+        Check success results.
+        """
         assert sum_solution.compute(1, 2) == 3
         assert sum_solution.compute(0, 0) == 0
 
     def test_invalid_input(self):
+        """
+        Check invalid inputs.
+
+        TODO: use fixture/factory
+        """
         assert sum_solution.compute(-1, 2) == None
         assert sum_solution.compute("a", 2) == None
         assert sum_solution.compute(10.3, 2) == None
         assert sum_solution.compute(101, 2) == None
         assert sum_solution.compute(1, 101) == None
-        
-
         
 
 
