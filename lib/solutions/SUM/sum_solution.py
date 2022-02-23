559d16b1316db276598f21d35ec4b6977fef957a
sbx_user1051 <sbx_user1051@169.254.3.69>
2022-02-23 01:16:43 +0000

    Wed Feb 23 01:16:43 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -5,13 +5,16 @@ def compute(x, y):
 
     param[0] = a positive integer between 0-100
     param[1] = a positive integer between 0-100
+    Return nothing if inputs are invalid
     """
     try:
         # Check type
         first_num = int(x)
         second_num = int(y)
 
-        if first_num < 0 or first_num > 100 or second_num < 0 or second_num > 100:
+        if first_num < 0 or first_num > 100:
+            return
+        if second_num < 0 or second_num > 100:
             return
 
     except ValueError:
@@ -19,5 +22,6 @@ def compute(x, y):
         return
 
     return first_num + second_num
+
 
 
